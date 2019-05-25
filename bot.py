4c8e4369eb5e5bb7b5d8d654e89aa87298a03640
AceAttorneyMaster111 <NoahS2003@gmail.com>
2019-05-25 10:16:39 -0400

    Finished hourly event loop

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 
 client = discord.Client()
 guild = None
-token = None
+token = ""
 with open("token.txt") as file:
 	token = file.read()
 
@@ -17,19 +17,22 @@ async def on_ready():
 async def check_for_users():
 	i=0
 	for member in guild.members:
-		if (discord.utils.find(lambda r: r.name == "Learners", guild.roles) not in member.roles):
+		if discord.utils.find(lambda r: r.name == "Learners", guild.roles) not in member.roles:
 			try:
 				await member.add_roles(discord.utils.find(lambda r: r.name == "Learners", guild.roles))
 				i += 1
 			except discord.Forbidden:
+				# #action-log
 				await client.get_channel(371388150683271178).send(":x: I need the **Manage Roles** permission to work correctly.")
 				return
-	await client.get_channel(371388150683271178).send(":white_check_mark: All users now have the Learner role! (%d added.)" % i)
+	if i > 0:
+		await client.get_channel(371388150683271178).send("Added Learners to %d users." % i)
 
 async def hour_loop():
-	if (client.is_ready()):
+	if client.is_ready():
 		await check_for_users()
 		await asyncio.sleep(60 * 60)
 	else:
 		await asyncio.sleep(60 * 5)
+client.loop.create_task(hour_loop())
 client.run(token)
